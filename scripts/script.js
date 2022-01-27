ef18ba53016ed70b249ed36729aaa3c847f61301
MychelAlbuquerque <mychelalbu@gmail.com>
2022-01-27 14:11:53 -0300

    feat: dark mode implementado

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+let dark = false;
+
+function ativou() {
+  if (!dark) {
+    dark = true;
+    document.body.style.setProperty('--background', '#33312F');
+    document.body.style.setProperty('--letra-escura', '#F5FBFD');
+    document.body.style.setProperty('--body', '#33312F');
+    
+  }else{
+    dark = false;
+    document.body.style.setProperty('--background', '#a3d9f026');
+    document.body.style.setProperty('--letra-escura', '#33312F');
+    document.body.style.setProperty('--body', '#fbfeffd8');
+  }
+
+
+}
