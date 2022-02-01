4c367a627df279f8e22ac9ff2f262908c0c3bb4d
MychelAlbuquerque <mychelalbu@gmail.com>
2022-02-01 00:46:00 -0300

    :recycle: refactor: improve dark mode
    CSS and JS changes to improve the dark mode
    added new colors and variables

--- darkMode.js
+++ darkMode.js
@@ -1,18 +1,55 @@
 let dark = false;
 
 function darkMode() {
+
+  let bodyDark = '#101418';
+  let bodyBright = '#ffffff';
+
+  let backgroundDark = '#0a0e16';
+  let backgroundBright = '#c2ecfd';
+
+  let shadowDarkMode = '#004768';
+  let shadowBrightMode = '#cad5da';
+
+  let logoDarkMode = '#cad5da';
+  let logoBrightMode = '--boxColor';
+
+  let buttonBrightMode = '#FF7816';
+  let buttonDarkMode = '#4967A3';
+
+  let letterDarkMode = '#F5FBFD';
+  let letterBrightMode = '#222222';
+
+
   if (!dark) {
     dark = true;
-    document.body.style.setProperty('--background', '#33312F');
-    document.body.style.setProperty('--letra-escura', '#F5FBFD');
-    document.body.style.setProperty('--body', '#33312F');
-    
-  }else{
+
+    /*change body color*/
+    document.body.style.setProperty('background-color', bodyDark);
+    /*change section color*/
+    document.body.style.setProperty('--background', backgroundDark);
+    /*change box shadow color*/
+    document.body.style.setProperty('--shadow', shadowDarkMode);
+    /*change logo color*/
+    document.body.style.setProperty('--logo', logoDarkMode);
+    /*change button color*/
+    document.body.style.setProperty('--button', buttonDarkMode);
+    /*change letter color*/
+    document.body.style.setProperty('--letter', letterDarkMode);
+  } else {
     dark = false;
-    document.body.style.setProperty('--background', '#a3d9f026');
-    document.body.style.setProperty('--letra-escura', '#33312F');
-    document.body.style.setProperty('--body', '#fbfeffd8');
+
+    /*change color body*/
+    document.body.style.setProperty('background-color', bodyBright);
+    /*change color sections*/
+    document.body.style.setProperty('--background', backgroundBright);
+    /*change color box shadow*/
+    document.body.style.setProperty('--shadow', shadowBrightMode);
+    /*change logo color*/
+    document.body.style.setProperty('--logo', logoBrightMode);
+    /*change button color*/
+    document.body.style.setProperty('--button', buttonBrightMode);
+    /*change letter color*/
+    document.body.style.setProperty('--letter', letterBrightMode);
   }
-
-
 }
